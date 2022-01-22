dca9b233423ce36b4fb6257741703bc4ac6c56b0
con-shaw <connor.shaw@hoistfinance.com>
2022-01-22 17:09:35 +0000

    logo

--- outlook-set-signature/src/taskpane/js/signature_templates.js
+++ outlook-set-signature/src/taskpane/js/signature_templates.js
@@ -52,12 +52,12 @@ function get_template_B_str(user_info)
 
   str += "<table>";
   str +=   "<tr>";
-  str +=     "<td style='border-right: 1px solid #000000; padding-right: 5px;'><img src='https://officedev.github.io/Office-Add-in-samples/Samples/outlook-set-signature/assets/sample-logo.png' alt='Logo' /></td>";
+  str +=     "<td style='border-right: 1px solid #000000; padding-right: 5px;'><img src='../../../hoist-finance.png' alt='Logo' /></td>";
   str +=     "<td style='padding-left: 5px;'>";
   str +=	   "<strong>" + user_info.name + "</strong>";
   str +=     is_valid_data(user_info.pronoun) ? "&nbsp;" + user_info.pronoun : "";
   str +=     "<br/>";
-  str +=	   user_info.email + "<br/>";
+  str +=	   user_info.job + "<br/>";
   str +=	   is_valid_data(user_info.phone) ? user_info.phone + "<br/>" : "";
   str +=     "</td>";
   str +=   "</tr>";
