bca2d81fc6d4a08f63327c22d53af28bfe4c6b21
con-shaw <connor.shaw@hoistfinance.com>
2022-02-07 10:24:47 +0000

    height

--- outlook-resource-booking/src/taskpane/js/powerapp.js
+++ outlook-resource-booking/src/taskpane/js/powerapp.js
@@ -4,7 +4,7 @@
     // The Office initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
       $(document).ready(function () {
-          var appId = "28d3e0f4-19b1-4f92-ad11-5b7b1dde0841";
+          var appId = "84ebf9de-0143-47b8-9b30-d662840efb4c";
         
           $('#canvas-iframe').attr("src", "https://apps.powerapps.com/play/" + appId);
       });
