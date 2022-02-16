071d196a9148d8226434e0c05bf99e51e173dad3
con-shaw <connor.shaw@hoistfinance.com>
2022-02-16 17:15:06 +0000

    uodate xml

--- outlook-resource-booking/src/taskpane/js/powerapp.js
+++ outlook-resource-booking/src/taskpane/js/powerapp.js
@@ -4,16 +4,6 @@
     Office.initialize = function (reason) {
       $(document).ready(function () {
 
-          windowHeight = $(window).height();
-          windowWidth = $(window).width();
-
-          $(window).resize(function() {
-            windowHeight = $(window).height();
-            windowWidth = $(window).width();
-
-            $(".temp").html("Width:" + windowWidth + ", Height:" + windowHeight + ", Reason:" + reason) ;
-          });
-
           var appId = "fc8de0d5-b57f-4e2f-8f2d-53ab19be03b6";
         
           $('#canvas-iframe').attr("src", "https://apps.powerapps.com/play/" + appId);
